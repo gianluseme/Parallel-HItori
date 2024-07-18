e58e32f4a8cbb91d4a523d311c217596497c52ad
Gianluigi Semeraro <gianluigi.semeraro@studenti.unisalento.it>
2024-07-18 03:16:40 +0200

    Added sequential fraction time detection

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ int main(int argc, char** argv) {
     int work_chunk_size = 0;
     bool benchmark_mode = false;
 
-    double seq_time_start, seq_time_end, total_seq_time = 0.0;
+    double seq_time_start, seq_time_end, total_seq_time;
 
     int option;
     while ((option = getopt(argc, argv, "n:pc:w:b")) != -1) {
@@ -120,15 +120,26 @@ int main(int argc, char** argv) {
 
     bool **visited = createVisitedMatrix(n);
 
-    double total_time = 0.0;
     double seq_work_time = 0.0;
 
     int iterations = benchmark_mode ? 10 : 1;
 
-    double local_total_seq_time = 0.0;
+    double total_seq_time_accumulated = 0.0;
+    double total_time_accumulated = 0.0;
+    double local_total_seq_time;
+
+    // Array per memorizzare i tempi sequenziali per ogni iterazione
+    double *seq_times = malloc(iterations * sizeof(double));
+    double *seq_work_times = malloc(iterations * sizeof(double));  // Array per seq_work_time
 
 
     for (int iter = 0; iter < iterations; iter++) {
+
+        total_seq_time_encode_stack = 0;
+        total_seq_time_request_work = 0;
+        total_seq_time_handle_requests = 0;
+        total_seq_time_split_work = 0;
+
         MPI_Barrier(MPI_COMM_WORLD);
         double start_time = MPI_Wtime();
 
@@ -136,41 +147,45 @@ int main(int argc, char** argv) {
         for (int i = 0; i < n; i++)
             MPI_Bcast(matrix[i], n, MPI_INT, 0, MPI_COMM_WORLD);
         seq_time_end = MPI_Wtime();
-        total_seq_time += (seq_time_end - seq_time_start);
+        total_seq_time = (seq_time_end - seq_time_start);
 
-        seq_work_time += generateConfigurations(matrix, n, visited, rank, size, stack_cutoff, work_chunk_size, compressed_state_type, benchmark_mode);
+        seq_work_time = generateConfigurations(matrix, n, visited, rank, size, stack_cutoff, work_chunk_size, compressed_state_type, benchmark_mode);
 
-        local_total_seq_time += total_seq_time_create_type + total_seq_time_initialize_grid + total_seq_time_print + total_seq_time_handle_requests + total_seq_time_split_work + total_seq_time_request_work + total_seq_time_encode_stack;
+        local_total_seq_time = total_seq_time_create_type + total_seq_time_initialize_grid + total_seq_time_print + total_seq_time_handle_requests + total_seq_time_split_work + total_seq_time_request_work + total_seq_time_encode_stack + total_seq_time;
 
+        seq_times[iter] = local_total_seq_time;
+        seq_work_times[iter] = seq_work_time;
         MPI_Barrier(MPI_COMM_WORLD);
         double end_time = MPI_Wtime();
 
-        if (rank == 0) {
-            double elapsed_time = end_time - start_time;
-            total_time += elapsed_time;
-            if (!benchmark_mode) {
-                printf("Parallel execution time: %f seconds\n", elapsed_time);
-            }
+        double elapsed_time = end_time - start_time;
+        total_time_accumulated += elapsed_time;
+
+        if (!benchmark_mode && rank == 0) {
+            printf("Parallel execution time: %f seconds\n", elapsed_time);
         }
     }
 
-    total_seq_time += seq_work_time / iterations;
-    total_time += total_seq_time;
-    total_time = total_time / iterations;
+    // Calcolo della media dei tempi sequenziali e seq_work_time
+    for (int iter = 0; iter < iterations; iter++) {
+        total_seq_time_accumulated += seq_times[iter];
+        seq_work_time += seq_work_times[iter];
+    }
+    total_seq_time_accumulated /= iterations;
+    seq_work_time /= iterations;
 
+    double avg_seq_work_time = seq_work_time / iterations;
+    double avg_total_time = total_time_accumulated / iterations;
 
     // Variabile per raccogliere il tempo sequenziale totale e il tempo totale
     double global_total_seq_time;
-
-    global_total_seq_time = local_total_seq_time;
-
-    global_total_seq_time +=total_seq_time;
+    global_total_seq_time = total_seq_time_accumulated + avg_seq_work_time;
 
     if (rank == 0 && benchmark_mode) {
 
         // Costruisci il comando per eseguire lo script Python
         char command[512];
-        snprintf(command, sizeof(command), "python3 ../benchmark.py %d %f %d results %f", size, total_time, n, global_total_seq_time);
+        snprintf(command, sizeof(command), "python3 ../benchmark.py %d %f %d results %f", size, avg_total_time, n, global_total_seq_time);
 
         // Esegui lo script Python
         execute_command(command);
@@ -178,7 +193,7 @@ int main(int argc, char** argv) {
 
     if (rank == 0 && size > 1) {
         printf("Total sequential time: %f seconds\n", global_total_seq_time);
-        printf("Total execution time: %f seconds\n", total_time);
+        printf("Total execution time: %f seconds\n", avg_total_time);
     }
 
     for (int i = 0; i < n; i++) {
@@ -190,9 +205,13 @@ int main(int argc, char** argv) {
 
     MPI_Type_free(&compressed_state_type);
 
+    free(seq_times);
+    free(seq_work_times);
+
 
     printf("Process %d: Finalizing\n", rank);
     MPI_Finalize();
+
     return 0;
 }
 
